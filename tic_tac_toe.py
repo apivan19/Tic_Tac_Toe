7523051d0d88f643595c6c4c4720a0fa2cfa0e83
Alexandru Paul Ivan <37052378+apivan19@users.noreply.github.com>
2020-08-26 17:35:08 -0400

    Create tic_tac_toe.py
    Added todo list to the file

---
+++ tic_tac_toe.py
@@ -0,0 +1,8 @@
+# Written in Python 3.7.7
+"""
+TODO: 
+1. Get it working w/ a random decision AI
+2. implement minmax algorithm for the computer
+3. Get the program working amongst two iterations of a single computer - socket programming
+4. Reformat the code into OOP / classes
+"""
